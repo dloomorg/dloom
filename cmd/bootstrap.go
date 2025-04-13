86dd6a5331f824bbe729285f7af141a60d01f4b5
pradipta <pradiptasarma@outlook.com>
2025-04-13 20:37:22 +0530

    Adds bootstrap, validations, bootstrap script, etc

---
+++ cmd/bootstrap.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"github.com/dloomorg/dloom/internal"
+	"github.com/spf13/cobra"
+)
+
+var bootstrapCmd = &cobra.Command{
+	Use:   "bootstrap [repository-url|directory]",
+	Short: "Clone a dotfile repository and install git if needed, or bootstrap system with an existing directory",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		target := args[0]
+		opts := internal.BootstrapOptions{
+			Config:   cfg,
+			target: target,
+		}
+		return internal.Bootstrap(opts, logger)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(bootstrapCmd)
+} 
